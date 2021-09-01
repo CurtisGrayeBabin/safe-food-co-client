a51dbb6950423394b2e6fffd10ed31b2030dfbc0
CurtisGrayeBabin <curtsocal@gmail.com>
2021-08-31 18:51:23 -0700

    Center page contents w/ redesign

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,18 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from "gatsby-plugin-image"
 
 const AboutPage = () => {
   return (
     <Layout pageTitle='About'>
-      <h1>About us:</h1>
-      <ul>
-        <li>Certified: Certified Instructor and Proctor for ServSafe</li>
-        <li>Professional: Certified Executive Chef (ACF certified)</li>
-        <li>Experienced: Instructed ServSafe courses at the college level for 6 years</li>
-      </ul>
+      <h1>About us</h1>
+      
+      <h2>Certified: Certified Instructor and Proctor for ServSafe</h2>
+      <StaticImage src='../images/servsafe_logo.png' alt='ServSafe logo image' width={300}/>
+      <h2>Professional: Certified Executive Chef (ACF certified)</h2>
+      <StaticImage src='../images/afc_badge.png' alt='ACF badge image' width={200}/>
+      <h2>Experienced: Instructed ServSafe courses at the college level for 6 years</h2>
+    
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,17 @@ import { StaticImage } from "gatsby-plugin-image"
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home'>
-      <h1>Food-Safe Solutions</h1>
-      
-      <h2>ServSafe exam proctoring</h2>
-      <h2>Proctoring for up to 5 people at a time</h2>
-      <h2>$100 per person</h2>
 
-      <StaticImage src='../images/afc_badge.png' alt='ACF badge image' width={200}/>
+  
+      <h1>Food-Safe Solutions</h1>
+      <h2>ServSafe exam proctoring!</h2>
       <StaticImage src='../images/servsafe_logo.png' alt='ServSafe logo image' width={300}/>
 
+      <h2>Proctoring for up to 5 people at a time</h2>
+      <h2>for $100 per person</h2>
+      <StaticImage src='../images/afc_badge.png' alt='ACF badge image' width={200}/>
+  
+      
 
     </Layout>
   )
