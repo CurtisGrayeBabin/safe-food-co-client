90595f720954711d79adb8a11784544b8c089587
CurtisGrayeBabin <curtsocal@gmail.com>
2021-09-10 13:47:28 -0700

    Reduce link border radius

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        'height': 'height',
+        'spacing': 'margin, padding',
+       }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      transitionProperty: ['hover', 'focus', 'responsive', 'motion-safe', 'motion-reduce'],
+      transform: ['motion-safe','hover', 'focus'],
+      animation: ['motion-safe'],
+      transitionTimingFunction: ['hover', 'focus'],
+      scale: ['active', 'group-hover'],
+    },
   },
   plugins: [],
 }
