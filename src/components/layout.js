757c112513d988afee5540a6acee4227335720b4
CurtisGrayeBabin <curtsocal@gmail.com>
2021-09-11 22:28:39 -0700

    Add basic react Helmet component to Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,18 @@
 import * as React from 'react'
 import { container } from '../styles/layout.module.css'
 import Navbar from './navbar'
+import { Helmet } from "react-helmet"
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <>
         <title>{pageTitle}</title>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{pageTitle}</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+        
         <Navbar></Navbar>
         <div className={container}>
             <main>
