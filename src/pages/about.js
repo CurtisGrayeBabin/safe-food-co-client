fc4b3c7a98c2c12e5e98487a0c6285e350037808
CurtisGrayeBabin <curtsocal@gmail.com>
2021-09-27 18:50:37 -0700

    Set font sizes in About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,8 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { ServSafeImg, ACFImg } from '../components/images'
 import {
-  bigText
+  bigText,
+  standardText
 } from '../styles/text.module.css'
 
 const AboutPage = () => {
@@ -10,15 +11,15 @@ const AboutPage = () => {
     <Layout pageTitle='About'>
       <h1 className={bigText}>About us</h1>
       
-      <h2>Certified: Certified Instructor and Proctor for ServSafe</h2>
+      <h2 className={standardText}>Certified: Certified Instructor and Proctor for ServSafe</h2>
       
       <ServSafeImg />
 
-      <h2>Professional: Certified Executive Chef (ACF certified)</h2>
+      <h2 className={standardText}>Professional: Certified Executive Chef (ACF certified)</h2>
 
       <ACFImg />
 
-      <h2>Experienced: Instructed ServSafe courses at the college level for 6 years</h2>
+      <h2 className={standardText}>Experienced: Instructed ServSafe courses at the college level for 6 years</h2>
     
     </Layout>
   )
